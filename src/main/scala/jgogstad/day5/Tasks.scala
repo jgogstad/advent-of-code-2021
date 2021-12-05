56206bc95eed77edd1b36fbcc0a60c6d1fa71274
Jostein Gogstad <jostein.gogstad@gmail.com>
2021-12-05 22:40:08 +0100

    add solution to day5

---
+++ src/main/scala/jgogstad/day5/Tasks.scala
@@ -0,0 +1,58 @@
+package jgogstad.day5
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.all._
+import fs2.Stream
+import fs2.io.file.{Files, Path}
+import io.odin.{Logger, consoleLogger}
+import jgogstad.utils._
+
+import scala.annotation.tailrec
+import scala.collection.immutable.MultiSet
+
+object Tasks extends IOApp {
+  val log: Logger[IO] = consoleLogger()
+
+  val input = Files[IO]
+    .readAll(Path(getClass.getClassLoader.getResource("day5/input.txt").getPath))
+    .through(fs2.text.utf8.decode)
+    .through(fs2.text.lines)
+    .evalMap {
+      case s"${comma(x1, y1)} -> ${comma(x2, y2)}" => ((x1, y1), (x2, y2)).pure[IO]
+      case s => IO.raiseError(new Exception(s"Unknown input: ${s}"))
+    }
+
+  def expand(p1: (Int, Int), p2: (Int, Int)): Stream[IO, (Int, Int)] = {
+    @tailrec
+    def go(acc: List[(Int, Int)], p1: (Int, Int), p2: (Int, Int)): List[(Int, Int)] = {
+      if (p1 === p2) acc
+      else {
+        val diffX = p2._1 - p1._1
+        val diffY = p2._2 - p1._2
+        val stepX = diffX / Math.max(1, Math.abs(diffX))
+        val stepY = diffY / Math.max(1, Math.abs(diffY))
+
+        val next = (p1._1 + stepX) -> (p1._2 + stepY)
+        go(next :: acc, next, p2)
+      }
+    }
+    Stream.emits(go(p1 :: Nil, p1, p2))
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val task1 = input.filter { case ((a, b), (c, d)) =>
+      a == c || b == d
+    }.flatMap((expand _).tupled)
+
+    val task2 = input.flatMap((expand _).tupled)
+
+    (solve(task1), solve(task2)).parTupled.flatMap(t2 => log.info(t2.show)).as(ExitCode.Success)
+  }
+
+  def solve(input: Stream[IO, (Int, Int)]): IO[Int] =
+    input
+      .fold(MultiSet.empty[(Int, Int)]: MultiSet[(Int, Int)]) { case (acc, el) => acc + el }
+      .map(_.occurrences.filter(_._2 >= 2).size)
+      .compile
+      .lastOrError
+}
--- src/main/scala/jgogstad/utils.scala
+++ src/main/scala/jgogstad/utils.scala
@@ -1,9 +1,18 @@
 package jgogstad
 
+import cats.syntax.all._
 import spire.math.SafeLong
 import spire.implicits._
+
+import scala.util.Try
 
 object utils {
   object long { def unapply(s: String): Option[Long] = s.toLongOption }
   object safeLong { def unapply(s: String): Option[SafeLong] = s.toLongOption.map(_.toSafeLong) }
+  object comma {
+    def unapply(s: String): Option[(Int, Int)] = s.split(",").toList match {
+      case h :: t :: Nil => (Try(h.toInt).toOption, Try(t.toInt).toOption).tupled
+      case _             => None
+    }
+  }
 }
