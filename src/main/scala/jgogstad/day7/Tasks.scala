c77a6e123d4319e88594794a1da68870e0ec5842
Jostein Gogstad <jostein.gogstad@gmail.com>
2021-12-08 09:00:23 +0100

    add solution for day7

---
+++ src/main/scala/jgogstad/day7/Tasks.scala
@@ -0,0 +1,42 @@
+package jgogstad.day7
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.all._
+import fs2.Stream
+import fs2.io.file.{Files, Path}
+import io.odin.{consoleLogger, Logger}
+import jgogstad.utils._
+import spire.math.SafeLong
+import spire.implicits._
+
+import scala.annotation.tailrec
+import scala.collection.immutable.MultiSet
+
+object Tasks extends IOApp {
+  val log: Logger[IO] = consoleLogger()
+
+  val input: IO[List[Int]] = Files[IO]
+    .readAll(Path(getClass.getClassLoader.getResource("day7/input.txt").getPath))
+    .through(fs2.text.utf8.decode)
+    .through(fs2.text.lines)
+    .map(_.split(",").map(_.toInt).toList)
+    .compile
+    .lastOrError
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    def fuelCost(crabs: List[Int])(cost: (Int, Int) => Int): Int =
+      (crabs.min to crabs.max).map(i => crabs.map(cost.curried(i)).sum).min
+
+    input.map(fuelCost).flatMap { cost =>
+      val task1 = cost((i, el) => Math.abs(i - el))
+      val task2 = cost { (i, el) =>
+        val d = Math.abs(i - el)
+        (d * (d + 1)) / 2
+      }
+
+      log.info(show"Task1: ${task1} Task 2: ${task2}").as(ExitCode.Success)
+    }
+
+  }
+
+}
