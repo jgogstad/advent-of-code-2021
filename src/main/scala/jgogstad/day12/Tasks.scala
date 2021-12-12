bff04550ba9ba04bf6e65a35d12d590bcae09189
Jostein Gogstad <jostein.gogstad@gmail.com>
2021-12-12 22:30:15 +0100

    add solution to day12

---
+++ src/main/scala/jgogstad/day12/Tasks.scala
@@ -0,0 +1,64 @@
+package jgogstad.day12
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.all._
+import fs2.Stream
+import fs2.io.file.{Files, Path}
+import io.odin.{consoleLogger, Logger}
+import jgogstad.utils.{clamp, CellularAutomata}
+import jgogstad._
+import org.jgrapht.graph.{DefaultUndirectedWeightedGraph, DefaultWeightedEdge, SimpleDirectedGraph, SimpleGraph}
+import spire.implicits._
+import spire.math.SafeLong
+
+import scala.annotation.tailrec
+import scala.collection.immutable.MultiSet
+import scala.collection.immutable.MultiDict
+
+object Tasks extends IOApp {
+  val log: Logger[IO] = consoleLogger()
+
+  val input = Files[IO]
+    .readAll(Path(getClass.getClassLoader.getResource("day12/input.txt").getPath))
+    .through(fs2.text.utf8.decode)
+    .through(fs2.text.lines)
+    .map { case s"$from-$to" => from -> to }
+
+  def mod_dfs(
+    acc: List[List[String]],
+    stack: List[(String, List[String], MultiSet[String])],
+    data: MultiDict[String, String]
+  ): List[List[String]] = {
+    stack match {
+      case Nil => acc
+      case (h, path, visitedSmall) :: t =>
+        if (h == "end") mod_dfs(("end" :: path).reverse :: acc, t, data)
+        else {
+          val newVisited = if (h.matches("[a-z]+")) visitedSmall + h else visitedSmall
+          val twice = newVisited.occurrences.toList.filter(_._2 > 1)
+
+//          task 1
+//          val expand = data.get(h).filterNot(s => visitedSmall.contains(s))
+          val expand = data.get(h).filter(s => !visitedSmall.contains(s) || twice.isEmpty || twice.exists(t2 => t2._1 == s && t2._2 < 2))
+
+          if (expand.isEmpty) mod_dfs(acc, t, data)
+          else mod_dfs(acc, expand.toList.map(el => (el, (h :: path), newVisited)) ++ t, data)
+        }
+    }
+  }
+
+  override def run(args: List[String]): IO[ExitCode] =
+    input.compile.toList.flatMap { (input: List[(String, String)]) =>
+      val edges = input
+        .flatMap { case (a, b) => List(a -> b, b -> a) }
+        .filterNot { case (a, b) => b == "start" || a == "end" }
+
+      val graph = MultiDict.from(edges)
+
+      val task1 = mod_dfs(Nil, ("start", Nil, MultiSet.empty[String]) :: Nil, 1, graph)
+      val task2 = mod_dfs(Nil, ("start", Nil, MultiSet.empty[String]) :: Nil, 2, graph)
+
+      log.info(show"task2: ${task2.size}")
+    }.as(ExitCode.Success)
+
+}
--- src/main/scala/jgogstad/package.scala
+++ src/main/scala/jgogstad/package.scala
@@ -2,8 +2,50 @@ import cats.Show
 
 import cats.syntax.all._
 import breeze.linalg.DenseMatrix
+import jgogstad.utils.clamp
+
+import scala.annotation.tailrec
 
 package object jgogstad {
   implicit def showIterable[F[_], A: Show](implicit ev: F[A] <:< IterableOnce[A]): Show[F[A]] = fa => ev(fa).map(_.show).iterator.mkString(",")
   implicit def showMatrix[A: Show]: Show[DenseMatrix[A]] = _.map(_.show).toString
+
+//  extension [A](matrix: DenseMatrix[A]) {
+//    def convolveAcc[S](z: S)(f: (S, DenseMatrix[A], (Int, Int)) => (S, A)): (S, DenseMatrix[A]) = {
+//      val kernel = DenseMatrix
+//      val shiftLeft = kernel.cols / 2
+//      val shiftUp = kernel.rows / 2
+//
+//      val centeredMask: List[(Int ,Int)] = kernel.activeIterator.toList
+//        .map { case (i, j) => (i - shiftUp, j - shiftLeft) }
+//
+//      matrix.mapPairs { case ((i, j), a) =>
+//        val realMask = centeredMask
+//          .map { case (r, c) => clamp(0, matrix.rows - 1)(i + r) -> clamp(0, matrix.cols - 1)(c + j) }
+//          .distinct
+//          .toList
+//        DenseMatrix.
+//      }
+//
+//      @tailrec
+//      def go(stack: List[(Int, Int)], s: S, acc: DenseMatrix[A]): (S, DenseMatrix[A]) =
+//        stack match {
+//          case Nil => s -> acc
+//          case (i, j) :: t =>
+//            val realMask = centeredMask
+//              .map { case (r, c) => clamp(0, matrix.rows - 1)(i + r) -> clamp(0, matrix.cols - 1)(c + j) }
+//              .distinct
+//              .toList
+//            val s1 = realMask.foldLeft(s) { case (s, (i, j)) =>
+//              val (s1, a) = f(s, i -> j, acc(i, j))
+//              acc.update(i, j, a)
+//              s1
+//            }
+//            go(t, s1, acc)
+//        }
+//
+//      go(indexes, z, matrix.copy)
+//
+//    }
+//  }
 }
